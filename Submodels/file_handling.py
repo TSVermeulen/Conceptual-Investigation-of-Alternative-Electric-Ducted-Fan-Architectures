b9a7542b721426215cba027c2974702b62e586b4
tsvermeulen <thomas0708.vermeulen@gmail.com>
2025-01-05 01:39:45 +0100

    Further work on MTFLO file generation tflow.xxx  Needs fixing of writing - typeError is currently raised.

--- Submodels/file_handling.py
+++ Submodels/file_handling.py
@@ -235,8 +235,16 @@ class fileHandling:
 
     class fileHandlingMTFLO:
 
-        def __init__(self):
-            pass
+        def __init__(self, *args) -> None:
+            """
+            
+            """
+
+            stage_count, case_name = args 
+
+            self.stage_count: int = stage_count
+            self.case_name: str = case_name
+
 
         def GetBladeParameters(self, 
                                design_params: dict,
@@ -265,10 +273,6 @@ class fileHandling:
                 - "thickness_data_points": Thickness data points along the blade profile.
                 - "geometric_blade_slope": Geometric blade slope distribution.
                 - "blade_slope_points": Blade slope data points.
-
-            TO DO:
-            ------
-            - Inclusion of blade twist into parameter calculations
             """
 
             # Initialize the airfoil parameterization class
@@ -317,12 +321,16 @@ class fileHandling:
 
         def ConstructBlades(self,
                             blading_params: dict,
-                            design_params: np.ndarray[dict]):
+                            design_params: np.ndarray[dict],
+                            ) -> dict:
             """
 
             interpolate between the airfoils to create the blade geometry inputs needed to create the MTFLO input file
             Uses a rectangular bivariate spine interpolation on (r,x) to obtain the blade geometry at each radial, and axial station 
-    
+
+            TO DO:
+            - Implement entropy calculation
+
             """
 
             # Collect blade geometry at each of the radial stations
@@ -352,63 +360,150 @@ class fileHandling:
             # Construct thickness and camber distribution rectangular data grid
             # Use a cosine spacing of data points to increase resolution at the leading and trailing edges
             # This is done to ensure that each profile is sampled at the same nondimensionalised points x/c, 
-            # enabling the creation of a rectangular grid for the bivariate spline interpolation
+            # enabling the creation of a rectangular grid for the bivariate spline interpolation.
+            # Uses 100 points to ensure an initial smooth interpolation. 
             n_points = 100
             cosine_space = (1 - np.cos(np.linspace(0, np.pi, n_points))) / 2
 
             # Loop over the different radial stations and compute the thickness and camber distribution for each station
             thickness_profile_distribution = np.zeros((len(design_params), n_points))
-            slope_distribution = np.zeros((len(design_params), n_points))
+            slope_profile_distribution = np.zeros((len(design_params), n_points))
             for station in range(len(thickness_profile_distribution)):
                 thickness_profile_distribution[station] = blade_geometry[station]["thickness_distr"](cosine_space)
-                slope_distribution[station] = blade_geometry[station]["blade_slope_distribution"](cosine_space)
+                slope_profile_distribution[station] = blade_geometry[station]["blade_slope_distribution"](cosine_space)
 
             # Construct the thickness and slope bivariate spline interpolations
             # First determine the appropriate interpolation method based on the minimum dimension of the data input
             # Then use the regulargridinterpolator to create the interpolation function
             method = 'quintic'
-            if 4 <= min(thickness_profile_distribution.shape, cosine_space) < 6:
+            if 4 <= min(min(thickness_profile_distribution.shape), cosine_space.size) < 6:
                 method = 'cubic'
-            elif min(thickness_profile_distribution.shape, cosine_space) < 4:
+            elif min(min(thickness_profile_distribution.shape), cosine_space.size) < 4:
                 method = 'linear'
 
-            thickness_distribution = interpolate.RegularGridInterpolator((cosine_space, 
-                                                                          blading_params["radial_stations"]),
+            thickness_distribution = interpolate.RegularGridInterpolator((blading_params["radial_stations"],
+                                                                          cosine_space),
                                                                          thickness_profile_distribution,
                                                                          method=method,
                                                                          ) 
             
             # Determine the appropriate interpolation method for the slope distribution
             method = 'quintic'
-            if 4 <= min(slope_distribution.shape, cosine_space) < 6:
+            if 4 <= min(min(slope_profile_distribution.shape), cosine_space.size) < 6:
                 method = 'cubic'
-            elif min(slope_distribution.shape, cosine_space) < 4:
+            elif min(min(slope_profile_distribution.shape), cosine_space.size) < 4:
                 method = 'linear'
 
-            slope_distribution = interpolate.RegularGridInterpolator((cosine_space, 
-                                                                      blading_params["radial_stations"]),
-                                                                     slope_distribution,
+            slope_distribution = interpolate.RegularGridInterpolator((blading_params["radial_stations"],
+                                                                      cosine_space),
+                                                                     slope_profile_distribution,
                                                                      method=method,
-                                                                     )
-            
+                                                                     )  
+
+            # CONSTRUCT DUMMY ENTROPY DISTRIBUTION
+            # This needs to be replaced with a proper entropy calculation, which is currently not implemented.
+            # Current implementation is a placeholder to allow for the construction of the MTFLO input file
+            entropy_profile_distribution = np.zeros((len(blading_params["radial_stations"]), len(cosine_space)))
+            entropy_distribution = interpolate.RegularGridInterpolator((blading_params["radial_stations"],
+                                                                       cosine_space),
+                                                                      entropy_profile_distribution,
+                                                                      method=method,
+                                                                      )     
+
             # Construct output data
             constructed_blade = {"chord_distribution": chord_distribution,
                                  "sweep_distribution": sweep_distribution,
                                  "twist_distribution": twist_distribution,
                                  "thickness_distribution": thickness_distribution,
                                  "slope_distribution": slope_distribution,
+                                 "entropy_distribution": entropy_distribution,
                                  }
             
             return constructed_blade
 
         def GenerateMTFLOInput(self,
-                               operating_conditions: dict,
+                               blading_params: np.ndarray[dict],
+                               design_params: np.ndarray[dict],
                                ) -> None:
             """
 
             Write the MTFLO input file for the given case
 
             """
+
+            filename = "tflow." + self.case_name
+            with open(filename, "w") as file:
+                # Write the case name to the file
+                file.write('NAME\n')
+                file.write(self.case_name + '\n')
+                file.write('END\n \n')
+
+                # Loop over the number of stages and write the data for each stage
+                for stage in range(self.stage_count):
+                    # First write the "generic" data for the stage
+                    # This includes the number of blades, the rotational rate, and the data types to be provided
+                    # Formatting is in-line with the user guide from the MTFLOW documentation
+
+                    # Start a stage block
+                    file.write('STAGE\n \n')
+
+                    # Write the number of blades
+                    file.write('NBLADE\n')
+                    file.write(blading_params[stage]["blade_count"] + '\n')
+                    file.write('END\n \n')
+
+                    # Write the rotational rate in units of omega * L / V
+                    file.write('OMEGA\n')
+                    file.write(blading_params[stage]["rotational_rate"] + '\n')
+                    file.write('END\n \n')
+
+                    # Write the data types to be provided for the stage
+                    file.write('DATYPE \n')
+                    file.write('x    r    T    Sr    DS\n')  # Use the x,r coordinates, together with thickness, blade slope, and entropy
+                    multipliers = [1., 1., 1., 1., 1.]  # Add multipliers for each data type
+                    additions = [0., 0., 0., 0., 0.]  # Add additions for each data type
+                    file.write('*' + '    '.join(map(str, multipliers)) + '\n')
+                    file.write('+' + '    '.join(map(str, additions)) + '\n')
+                    file.write('END\n \n')
+
+                    # Collect the blade geometry interpolations
+                    blade_geometry: dict = self.ConstructBlades(blading_params[stage], 
+                                                                design_params[stage])
+                    
+                    # Generate interpolated data to construct the file geometry
+                    n_axial_points = 50
+                    n_radial_points = 20
+                    axial_points = np.linspace(0, 1, n_axial_points)
+                    radial_points = np.linspace(0, 1, n_radial_points)
+
+                    # Loop over the radial points and construct the data for each radial point
+                    # Each radial point is defined as a "section" within the input file
+                    for i in range(n_radial_points): 
+                        # Create a section in the input file
+                        file.write('SECTION\n')
+
+                        # Loop over the streamwise points and construct the data for each streamwise point
+                        # Each data point consists of the data [x, r, T, sRel, and dS]
+                        for j in range(len(axial_points)):     
+                            row = np.array([axial_points[j],
+                                            radial_points[i],
+                                            blade_geometry["thickness_distribution"]((radial_points[i], axial_points[j])),
+                                            blade_geometry["slope_distribution"]((radial_points[i], axial_points[j])),
+                                            blade_geometry["entropy_distribution"]((radial_points[i], axial_points[j])),
+                                            ])
+                            
+                            # Write the row to the file
+                            file.write('    '.join(map(str, row)) + '\n')
+
+                        # End the radial section
+                        file.write('END\n')
+                        
+
+                    
+
+
+
+
 
             return None
 
@@ -423,6 +518,8 @@ if __name__ == "__main__":
     call_class.GenerateMTSETInput()
 
 
-    call_class = fileHandling.fileHandlingMTFLO()
-    test = call_class.ConstructBlades({"radial_stations": 10, "chord_length": 1.0}, [n2415_coeff, n2415_coeff])
+    call_class = fileHandling.fileHandlingMTFLO(1, "test_case")
+    test = call_class.ConstructBlades({"radial_stations": [0, 1], "chord_length": [0.2, 0.2], "sweep_angle":[0, 0], "twist_angle": [0, 0]}, [n2415_coeff, n2415_coeff])
     
+    generate_test = call_class.GenerateMTFLOInput([{"stage_count": 1, "blade_count": 18, "radial_stations": [0, 1], "chord_length": [0.2, 0.2], "sweep_angle":[0, 0], "twist_angle": [0, 0]}], [n2415_coeff, n2415_coeff])
+    print(generate_test)
