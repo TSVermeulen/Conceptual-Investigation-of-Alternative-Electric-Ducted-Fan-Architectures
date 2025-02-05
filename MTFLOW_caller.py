bd928a039e2a567f18051ada1ef138d9feafd0df
Thomas Stephan Vermeulen <thomas0708.vermeulen@gmail.com>
2025-02-05 23:01:06 +0100

    Documentation update

--- MTFLOW_caller.py
+++ MTFLOW_caller.py
@@ -1,6 +1,80 @@
 """
+MTFLOW_caller
+=================
 
+Description
+-----------
+This module provides a complete interface with the MTFLOW programs, from input 
+file generation through to output generation, including convergence handling, 
+choking handling, crash detection, and grid refinement. It builds on the 
+MTSET_call, MTFLO_call, and MTSOL_call classes to construct the complete interface.
+The module is able to maintain a logging file to allow for debugging through the 
+use of an optional control boolean. 
 
+Classes
+-------
+ExitFlag
+    A class with exit flags of the MTSOL interface to help with debugging. Effectively a copy of the MTSOL_call.ExitFlag() class
+MTFLOW_caller
+    A class to execute the complete MTFLOW interface. Effectively a detailed "wrapper" class of the MTSET_call, MTFLO_call, 
+    and MTSOL_call classes. 
+
+Examples
+--------
+>>> import time
+
+>>> oper = {"Inlet_Mach": 0.6,
+>>>         "Inlet_Reynolds": 5e6,
+>>>         "N_crit": 9,
+>>>         }
+
+>>> analysisName = "test_case"
+    
+>>> # Roughly basing the blade design on the CFM Leap engine (approximate chord lengths)
+>>> blading_parameters = [{"root_LE_coordinate": 0.5, "rotational_rate": 0.75, "blade_count": 15, "radial_stations": [0.1, 1.15], "chord_length": [0.3, 0.2], "sweep_angle":[np.pi/16, np.pi/16], "twist_angle": [0, np.pi/8]},
+>>>                       {"root_LE_coordinate": 1.1, "rotational_rate": 0., "blade_count": 15, "radial_stations": [0.1, 1.3], "chord_length": [0.15, 0.1], "sweep_angle":[np.pi/16, np.pi/16], "twist_angle": [0, np.pi/8]}]
+    
+>>> # Model the fan and stator blades using a uniform naca2415 profile along the blade span
+>>> n2415_coeff = {"b_0": 0.20300919575972556, "b_2": 0.31901972386590877, "b_8": 0.04184620466207193, "b_15": 0.7500824561993612, "b_17": 0.6789808614463232, "x_t": 0.298901583, "y_t": 0.060121131, "x_c": 0.40481558571382253, "y_c": 0.02025376839986754, "z_TE": -0.0003399582707130648, "dz_TE": 0.0017, "r_LE": -0.024240593156029916, "trailing_wedge_angle": 0.16738688797915346, "trailing_camberline_angle": 0.0651960639817597, "leading_edge_direction": 0.09407653642497815}
+>>> design_parameters = [[n2415_coeff, n2415_coeff],
+>>>                      [n2415_coeff, n2415_coeff]]
+    
+>>> # Model the duct using a naca 6409 profile
+>>> duct_parameters = {"b_0": 0.07979831, "b_2": 0.20013347, "b_8": 0.02901246, "b_15": 0.74993802, "b_17": 0.78496242, 'x_t': 0.30429947838135246, 'y_t': 0.0452171520304373, 'x_c': 0.4249653844429819, 'y_c': 0.06028051002570214, 'z_TE': -0.0003886462495685791, 'dz_TE': 0.0004425237127035188, 'r_LE': -0.009225474218611841, 'trailing_wedge_angle': 0.10293203348896998, 'trailing_camberline_angle': 0.21034003141636426, 'leading_edge_direction': 0.26559481057525414, "Chord Length": 3.0, "Leading Edge Coordinates": (0, 1.2)}
+    
+>>> # Model the centrebody using a naca 0025 profile
+>>> centrebody_parameters = {"b_0": 0., "b_2": 0., "b_8": 7.52387039e-02, "b_15": 7.46448823e-01, "b_17": 0, 'x_t': 0.29842005729819904, 'y_t': 0.12533559300869632, 'x_c': 0, 'y_c': 0, 'z_TE': 0, 'dz_TE': 0.00277173368735548, 'r_LE': -0.06946118699675888, 'trailing_wedge_angle': np.float64(0.27689037361278407), 'trailing_camberline_angle': 0.0, 'leading_edge_direction': 0.0, "Chord Length": 4, "Leading Edge Coordinates": (0.3, 0)}
+
+>>> start_time = time.time()
+>>> class_call = MTFLOW_caller(operating_conditions=oper,
+>>>                         #    centrebody_params=centrebody_parameters,
+>>>                            duct_params=duct_parameters,
+>>>                            blading_parameters=blading_parameters,
+>>>                            design_parameters=design_parameters,
+>>>                            analysis_name=analysisName,
+>>>                            ).caller()
+>>> end_time = time.time()
+    
+>>> print(f"Execution of MTFLOW_call.caller() took {end_time - start_time} second")
+
+Notes
+-----
+N/A
+
+References
+----------
+The required input data, limitations, and structures are documented within the MTFLOW user manual:
+https://web.mit.edu/drela/Public/web/mtflow/mtflow.pdf
+
+Versioning
+----------
+Author: T.S. Vermeulen
+Email: T.S.Vermeulen@student.tudelft.nl
+Student ID 4995309
+Version: 1.0
+
+Changelog:
+- V1.0: Adapted version of a parameterization using only the bezier coefficients to give an improved fit to the reference data. 
 
 """
 
@@ -8,6 +82,7 @@ import sys
 import os
 import logging
 import random
+import numpy as np
 from enum import Enum
 
 # Enable submodel relative imports 
@@ -173,6 +248,7 @@ class MTFLOW_caller:
         -------
         None
         """
+
         try:
             # --------------------
             # Set up logging for the caller execution
@@ -311,7 +387,7 @@ class MTFLOW_caller:
 
 
 if __name__ == "__main__":
-    import numpy as np
+    import time
 
     # Define some test inputs
     oper = {"Inlet_Mach": 0.6,
@@ -336,6 +412,7 @@ if __name__ == "__main__":
     # Model the centrebody using a naca 0025 profile
     centrebody_parameters = {"b_0": 0., "b_2": 0., "b_8": 7.52387039e-02, "b_15": 7.46448823e-01, "b_17": 0, 'x_t': 0.29842005729819904, 'y_t': 0.12533559300869632, 'x_c': 0, 'y_c': 0, 'z_TE': 0, 'dz_TE': 0.00277173368735548, 'r_LE': -0.06946118699675888, 'trailing_wedge_angle': np.float64(0.27689037361278407), 'trailing_camberline_angle': 0.0, 'leading_edge_direction': 0.0, "Chord Length": 4, "Leading Edge Coordinates": (0.3, 0)}
 
+    start_time = time.time()
     class_call = MTFLOW_caller(operating_conditions=oper,
                                centrebody_params=centrebody_parameters,
                                duct_params=duct_parameters,
@@ -343,3 +420,6 @@ if __name__ == "__main__":
                                design_parameters=design_parameters,
                                analysis_name=analysisName,
                                ).caller()
+    end_time = time.time()
+
+    print(f"Execution of MTFLOW_call.caller() took {end_time - start_time} second")
